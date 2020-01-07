f96feb6088b209899e04849937ab4d602f994a66
SnzDev <tms.working@gmail.com>
2020-01-07 13:31:03 -0300

    Creating login page Creating app.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <div className="login-box">
+        <div className="email">
+          <label htmlFor="email">E-mail: </label>
+          <input
+          id="email"
+          type="email"
+          placeholder="example@youremail.com"
+          />
+        </div>
+        
+        <div className="password">
+          <label htmlFor="password">Password: </label>
+          <input
+          id="password"
+          type="password"
+          placeholder="********"
+          />
+        </div>
+
+      </div>
+    </>
   );
 }
 
